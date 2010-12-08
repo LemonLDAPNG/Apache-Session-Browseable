b3af80ccd1fc33599effb160188c8dfd02cd364d
guimard <guimard@1dbb9719-a921-0410-b57f-c3a383c2c641>
2010-12-08 17:00:31 +0000

    Update versions
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/lemonldap/trunk/contribs/apache-session-browseable@1908 1dbb9719-a921-0410-b57f-c3a383c2c641

--- lib/Apache/Session/Browseable/Redis.pm
+++ lib/Apache/Session/Browseable/Redis.pm
@@ -8,7 +8,7 @@ use Apache::Session::Generate::MD5;
 use Apache::Session::Lock::Null;
 use Apache::Session::Serialize::Base64;
 
-our $VERSION = '0.1';
+our $VERSION = '0.2';
 our @ISA     = qw(Apache::Session);
 
 sub populate {
