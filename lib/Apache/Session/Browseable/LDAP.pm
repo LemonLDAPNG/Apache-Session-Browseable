fdb96005497d0ede526f0a7a329ae5aaa1566555
xguimard <x.guimard@free.fr>
2013-07-26 13:47:28 +0200

    escape_filter_value must be imported explicitely

--- lib/Apache/Session/Browseable/LDAP.pm
+++ lib/Apache/Session/Browseable/LDAP.pm
@@ -8,7 +8,7 @@ use Apache::Session::Browseable::Store::LDAP;
 use Apache::Session::Generate::MD5;
 use Apache::Session::Serialize::Base64;
 use Apache::Session::Browseable::_common;
-use Net::LDAP::Util;
+use Net::LDAP::Util qw(escape_filter_value);
 
 our $VERSION = '1.0';
 our @ISA     = qw(Apache::Session Apache::Session::Browseable::_common);
