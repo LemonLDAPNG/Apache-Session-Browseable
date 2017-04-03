a8a235c3b362f60d9e0d2109b6464de3f870c988
Xavier Guimard <x.guimard@free.fr>
2017-04-03 23:53:11 +0200

    Cast indexes

--- lib/Apache/Session/Browseable/PgHstore.pm
+++ lib/Apache/Session/Browseable/PgHstore.pm
@@ -176,7 +176,7 @@ Optionaly, add indexes on some fields. Example for Lemonldap::NG:
 
   CREATE INDEX uid1 ON sessions ( (a_session -> '_whatToTrace') );
   CREATE INDEX  s1  ON sessions ( (a_session -> '_session_kind') );
-  CREATE INDEX  u1  ON sessions ( (a_session -> '_utime') );
+  CREATE INDEX  u1  ON sessions ( ( cast(a_session -> '_utime' AS int) ) );
   CREATE INDEX ip1  ON sessions ( (a_session -> 'ipAddr') );
 
 Use it like L<Apache::Session::Browseable::Postgres> except that you don't
