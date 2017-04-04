5de16042461f87919a9ea24b15499a747d7b8971
Xavier Guimard <x.guimard@free.fr>
2017-04-04 07:15:11 +0200

    Prepare v 1.2.5

--- lib/Apache/Session/Browseable/PgJSON.pm
+++ lib/Apache/Session/Browseable/PgJSON.pm
@@ -8,7 +8,7 @@ use Apache::Session::Browseable::Store::Postgres;
 use Apache::Session::Generate::SHA256;
 use Apache::Session::Serialize::JSON;
 
-our $VERSION = '1.2.2';
+our $VERSION = '1.2.5';
 our @ISA     = qw(Apache::Session);
 
 sub populate {
