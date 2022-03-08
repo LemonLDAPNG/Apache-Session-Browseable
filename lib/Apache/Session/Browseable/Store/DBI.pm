1288c777a2be536bb4742a6dc998742ade9d20cc
Yadd <yadd@debian.org>
2022-03-08 14:49:05 +0100

    Releasing v1.3.10

--- lib/Apache/Session/Browseable/Store/DBI.pm
+++ lib/Apache/Session/Browseable/Store/DBI.pm
@@ -3,7 +3,7 @@ package Apache::Session::Browseable::Store::DBI;
 use strict;
 use Apache::Session::Store::DBI;
 our @ISA     = qw(Apache::Session::Store::DBI);
-our $VERSION = 1.2.2;
+our $VERSION = 1.3.10;
 
 sub insert {
     my ( $self, $session ) = @_;
